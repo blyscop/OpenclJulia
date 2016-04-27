db830279cf5ad0cdd842d5ba8be9830507002421
blyscop <rikudou6@live.fr>
2016-04-27 11:29:13 +0200

    Initial Commit

---
+++ titi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  titi
+//
+//  Created by Blyscop on 27/04/2016.
+//  Copyright © 2016 Blyscop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
